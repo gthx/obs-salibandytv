94165a12ffe8cb3284fcd919f7b83462bec1c1d7
Tuomas Harju <tuomash@unity3d.com>
2023-09-06 16:56:52 +0300

    add hardcoded Rangers-Tiikerit game

--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -8,11 +8,11 @@ export function Scoreboard() {
       <div class={styles.Container}>
         <img class={styles.Logo} src='https://kirkkonummirangers.fi/wp-content/uploads/2022/05/logo.png' />
         <div class={styles.HomeColor}></div>
-        <div class={styles.HomeTeam}>{"RAN"}</div>
+        <div class={styles.HomeTeam}>{"RANG"}</div>
         <div class={styles.HomeScore}>{score.home}</div>
-        <img class={styles.Logo} src='https://www.hawks.fi/wp-content/uploads/2021/08/Hawks_Logo_200x200.png' />
+        <img class={styles.Logo} src='https://upload.wikimedia.org/wikipedia/fi/8/89/Tikkurilan_Tiikerit.png' />
         <div class={styles.AwayColor}></div>
-        <div class={styles.AwayTeam}>{"HAW"}</div>
+        <div class={styles.AwayTeam}>{"TIIK"}</div>
         <div class={styles.AwayScore}>{score.away}</div>
         <div class={styles.Period}>{score.periodText}</div>
         <div class={styles.Time}>{score.timeText}</div>
