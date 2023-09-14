120722c7c63b84bbc503074412b2622f705a02da
Tuomas Harju <tuomash@unity3d.com>
2023-09-14 23:34:36 +0300

    fix logos

--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -1,17 +1,19 @@
 import styles from './Scoreboard.module.css';
 import { useScore } from './ScoreContext';
+import homeLogo from './assets/logos/rangers.png';
+import awayLogo from './assets/logos/koovee.png';
 
 export function Scoreboard() {
   const [score] = useScore();
   return (
     <div class={styles.Scoreboard}>
       <div class={styles.Container}>
-        <img class={styles.Logo} src='src/assets/logos/rangers.png' />
+        <img class={styles.Logo} src={homeLogo} />
         <div class={styles.HomeColor}></div>
         <div class={styles.HomeTeam}>{"RAN"}</div>
         <div class={styles.HomeScore}>{score.home}</div>
         <div class={styles.Divider}>-</div>
-        <img class={styles.Logo} src='src/assets/logos/koovee.png' />
+        <img class={styles.Logo} src={awayLogo} />
         <div class={styles.AwayColor}></div>
         <div class={styles.AwayTeam}>{"KOO"}</div>
         <div class={styles.AwayScore}>{score.away}</div>
