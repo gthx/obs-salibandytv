72768f4e3b3f480b7bc625662b9dd6f681209dbe
Tuomas Harju <tuomash@unity3d.com>
2023-09-30 13:26:03 +0300

    Rangers-SalBa

--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -1,7 +1,7 @@
 import styles from './Scoreboard.module.css';
 import { useScore } from './ScoreContext';
 import homeLogo from './assets/logos/rangers.png';
-import awayLogo from './assets/logos/koovee.png';
+import awayLogo from './assets/logos/salba.png';
 
 export function Scoreboard() {
   const [score] = useScore();
@@ -15,7 +15,7 @@ export function Scoreboard() {
         <div class={styles.Divider}>-</div>
         <img class={styles.Logo} src={awayLogo} />
         <div class={styles.AwayColor}></div>
-        <div class={styles.AwayTeam}>{"KOO"}</div>
+        <div class={styles.AwayTeam}>{"SAL"}</div>
         <div class={styles.AwayScore}>{score.away}</div>
         <div class={styles.Period}>{score.periodText}</div>
         <div class={styles.Time}>{score.timeText}</div>
