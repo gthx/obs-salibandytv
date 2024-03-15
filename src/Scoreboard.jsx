483c14548e6f360f8674fc9b3fea8f20b63325b0
Tuomas Harju <tuomash@unity3d.com>
2024-03-15 22:49:16 +0200

    Rangers-FBC

--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -1,20 +1,20 @@
 import styles from './Scoreboard.module.css';
 import { useScore } from './ScoreContext';
-//import homeLogo from './assets/logos/rangers.webp'
+import homeLogo from './assets/logos/rangers.webp'
 
 export function Scoreboard() {
   const [score] = useScore();
   return (
     <div class={styles.Scoreboard}>
       <div class={styles.Container}>
-        <img class={styles.Logo} src={"https://cdn.torneopal.net/logo/salibandy/757x.webp"} />
+        <img class={styles.Logo} src={homeLogo} />
         <div class={styles.HomeColor}></div>
-        <div class={styles.HomeTeam}>{"WI"}</div>
+        <div class={styles.HomeTeam}>{"RAN"}</div>
         <div class={styles.HomeScore}>{score.home}</div>
         <div class={styles.Divider}>-</div>
-        <img class={styles.Logo} src={"https://cdn.torneopal.net/logo/salibandy/374x.webp"} />
+        <img class={styles.Logo} src={"https://cdn.torneopal.net/logo/salibandy/800x.webp"} />
         <div class={styles.AwayColor}></div>
-        <div class={styles.AwayTeam}>{"KRP"}</div>
+        <div class={styles.AwayTeam}>{"FBC"}</div>
         <div class={styles.AwayScore}>{score.away}</div>
         <div class={styles.Period}>{score.periodText}</div>
         <div class={styles.Time}>{score.timeText}</div>
